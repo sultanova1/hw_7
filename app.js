bcbe54347b76815afeb0319a0fbe55a5dab2b88f
Aida <sultanovaaida29@gmail.com>
2023-10-28 15:36:30 +0600

    lesson_7

---
+++ app.js
@@ -0,0 +1,19 @@
+     const  buttons =document.querySelectorAll('.show-answer');
+buttons.forEach(button=>{
+    button.addEventListener('click',()=>{
+        const answer= button.nextElementSibling;
+        if (answer.style.display === 'none') {
+            answer.style.display = 'block';
+            button.textContent = 'Скрыть ответ';
+        } else {
+            answer.style.display = 'none'
+            button.textContent = 'Показать ответ';
+        }
+    });
+     });
+
+
+
+
+
+
